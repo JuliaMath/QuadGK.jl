32fe44d7751974440ff984b83effebab325e9889
Alex Arslan <ararslan@comcast.net>
2016-12-23 13:36:07 -0800

    (Attempt to) fix doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,11 @@
 using Documenter, QuadGK
 
 makedocs(
+    modules = [QuadGK],
+    clean = false,
     format = :html,
     sitename = "QuadGK.jl",
+    authors = "Steven G. Johnson and contributors.",
     pages = [
         "Home" => "index.md",
     ],
@@ -11,6 +14,4 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaMath/QuadGK.jl.git",
     target = "build",
-    deps = nothing,
-    make = nothing,
 )
