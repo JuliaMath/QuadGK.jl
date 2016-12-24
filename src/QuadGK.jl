d2ad74333b1ea70af63f206f396db4714beed3d0
Alex Arslan <ararslan@comcast.net>
2016-12-23 17:58:24 -0800

    Update Kronrod docstring

--- src/QuadGK.jl
+++ src/QuadGK.jl
@@ -354,6 +354,20 @@ The function Also computes the embedded `n`-point Gauss quadrature weights `gw`
 
 `T` is an optional parameter specifying the floating-point type, defaulting
 to `Float64`. Arbitrary precision (`BigFloat`) is also supported.
+
+Given these points and weights, the estimated integral `I` and error `E` can
+be computed for an integrand `f(x)` as follows:
+
+    x, w, wg = kronrod(n)
+    fx⁰ = f(x[end])                # f(0)
+    x⁻ = x[1:end-1]                # the x < 0 Kronrod points
+    fx = f.(x⁻) .+ f.((-).(x⁻))    # f(x < 0) + f(x > 0)
+    I = sum(fx .* w[1:end-1]) + fx⁰ * w[end]
+    if isodd(n)
+        E = abs(sum(fx[2:2:end] .* wg[1:end-1]) + fx⁰*wg[end] - I)
+    else
+        E = abs(sum(fx[2:2:end] .* wg[1:end])- I)
+    end
 """
 function kronrod{T<:AbstractFloat}(::Type{T}, n::Integer)
     if n < 1
