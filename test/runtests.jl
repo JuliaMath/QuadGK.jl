f993f5319471ca5aa5a84344ba4fea985c0d4656
Yichao Yu <yyc1992@gmail.com>
2017-02-12 18:28:27 -0500

    Fix depwarns on 0.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,8 @@ import QuadGK: quadgk, gauss, kronrod
     @test quadgk(cos, 0,0.7,1)[1] ≈ sin(1)
     @test quadgk(x -> exp(im*x), 0,0.7,1)[1] ≈ (exp(1im)-1)/im
     @test quadgk(x -> exp(im*x), 0,1im)[1] ≈ -1im*expm1(-1)
-    @test_approx_eq_eps quadgk(cos, 0,BigFloat(1),order=40)[1] sin(BigFloat(1)) 1000*eps(BigFloat)
+    @test isapprox(quadgk(cos, 0,BigFloat(1),order=40)[1], sin(BigFloat(1)),
+                   atol=1000*eps(BigFloat))
     @test quadgk(x -> exp(-x), 0,0.7,Inf)[1] ≈ 1.0
     @test quadgk(x -> exp(x), -Inf,0)[1] ≈ 1.0
     @test quadgk(x -> exp(-x^2), -Inf,Inf)[1] ≈ sqrt(pi)
