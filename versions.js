ce0ee9569d29112706cb94d551e8247e0495b34e
autodocs <autodocs>
2018-07-03 19:48:06 +0000

    build based on e20764a

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-2.0",
   "release-1.0",
   "release-0.2",
   "release-0.1",
   "release-0.0",
+  "v2.0.0",
   "v1.0.0",
   "v0.2.1",
   "v0.2.0",
