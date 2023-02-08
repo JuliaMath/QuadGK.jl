3d06488a4d026b909bf0900e52114a3b3c059dd2
Steven G. Johnson <stevenj@alum.mit.edu>
2023-02-07 22:12:26 -0500

    order=1 bugfix

--- src/evalrule.jl
+++ src/evalrule.jl
@@ -16,24 +16,20 @@ function evalrule(f::F, a,b, x,w,gw, nrm) where {F}
     # Ik and Ig are integrals via Kronrod and Gauss rules, respectively
     s = convert(eltype(x), 0.5) * (b-a)
     n1 = 1 - (length(x) & 1) # 0 if even order, 1 if odd order
-    # unroll first iterationof loop to get correct type of Ik and Ig
-    fg = f(a + (1+x[2])*s) + f(a + (1-x[2])*s)
-    fk = f(a + (1+x[1])*s) + f(a + (1-x[1])*s)
-    Ig = fg * gw[1]
-    Ik = fg * w[2] + fk * w[1]
-    for i = 2:length(gw)-n1
+    if n1 == 0 # even: Gauss rule does not include x == 0
+        Ik = f(a + s) * w[end]
+        Ig = zero(Ik)
+    else # odd: don't count x==0 twice in Gauss rule
+        f0 = f(a + s)
+        Ig = f0 * gw[end]
+        Ik = f0 * w[end] +
+            (f(a + (1+x[end-1])*s) + f(a + (1-x[end-1])*s)) * w[end-1]
+    end
+    for i = 1:length(gw)-n1
         fg = f(a + (1+x[2i])*s) + f(a + (1-x[2i])*s)
         fk = f(a + (1+x[2i-1])*s) + f(a + (1-x[2i-1])*s)
         Ig += fg * gw[i]
         Ik += fg * w[2i] + fk * w[2i-1]
-    end
-    if n1 == 0 # even: Gauss rule does not include x == 0
-        Ik += f(a + s) * w[end]
-    else # odd: don't count x==0 twice in Gauss rule
-        f0 = f(a + s)
-        Ig += f0 * gw[end]
-        Ik += f0 * w[end] +
-            (f(a + (1+x[end-1])*s) + f(a + (1-x[end-1])*s)) * w[end-1]
     end
     Ik_s, Ig_s = Ik * s, Ig * s # new variable since this may change the type
     E = nrm(Ik_s - Ig_s)
@@ -57,25 +53,21 @@ function evalrule(f::InplaceIntegrand{F}, a,b, x,w,gw, nrm) where {F}
     s = convert(eltype(x), 0.5) * (b-a)
     n1 = 1 - (length(x) & 1) # 0 if even order, 1 if odd order
     fg, fk, Ig, Ik = f.fg, f.fk, f.Ig, f.Ik # pre-allocated temporary arrays
-    # unroll first iterationof loop to get correct type of Ik and Ig
-    eval2x!(fg, f, a + (1+x[2])*s, a + (1-x[2])*s)
-    eval2x!(fk, f, a + (1+x[1])*s, a + (1-x[1])*s)
-    Ig .= fg .* gw[1]
-    Ik .= fg .* w[2] .+ fk .* w[1]
-    for i = 2:length(gw)-n1
+    f.f!(f.fx, a + s)
+    if n1 == 0 # even: Gauss rule does not include x == 0
+        Ik .= f.fx .* w[end]
+        Ig .= zero.(Ik)
+    else # odd: don't count x==0 twice in Gauss rule
+        Ig .= f.fx .* gw[end]
+        f.f!(f.fg, a + (1+x[end-1])*s)
+        f.f!(f.fk, a + (1-x[end-1])*s)
+        Ik .= f.fx .* w[end] .+ (f.fg + f.fk) .* w[end-1]
+    end
+    for i = 1:length(gw)-n1
         eval2x!(fg, f, a + (1+x[2i])*s, a + (1-x[2i])*s)
         eval2x!(fk, f, a + (1+x[2i-1])*s, a + (1-x[2i-1])*s)
         Ig .+= fg .* gw[i]
         Ik .+= fg .* w[2i] .+ fk .* w[2i-1]
-    end
-    f.f!(f.fx, a + s)
-    if n1 == 0 # even: Gauss rule does not include x == 0
-        Ik .+= f.fx .* w[end]
-    else # odd: don't count x==0 twice in Gauss rule
-        Ig .+= f.fx .* gw[end]
-        f.f!(f.fg, a + (1+x[end-1])*s)
-        f.f!(f.fk, a + (1-x[end-1])*s)
-        Ik .+= f.fx .* w[end] .+ (f.fg + f.fk) .* w[end-1]
     end
     Ik_s = Ik * s # new variable since this may change the type
     f.Idiff .= Ik_s .- Ig .* s
