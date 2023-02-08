b4803e50f03fa3c0db69b1fc73d7a38a00a0f652
Steven G. Johnson <stevenj@mit.edu>
2023-02-08 16:31:02 -0500

    typo in docstring

--- src/adapt.jl
+++ src/adapt.jl
@@ -270,7 +270,7 @@ Identical to [`quadgk`](@ref) but returns a triple `(I, E, count)`
 of the estimated integral `I`, the estimated error bound `E`, and a `count`
 of the number of times the integrand `f` was evaluated.
 
-The count of integrand evaluations is a useful performance metric, a large
+The count of integrand evaluations is a useful performance metric: a large
 number typically indicates a badly behaved integrand (with singularities,
 discontinuities, sharp peaks, and/or rapid oscillations), in which case
 it may be possible to mathematically transform the problem in some way
